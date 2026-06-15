8896a34654f3320c1eca109d2b04c72c5e8214cb
Awang Pramana <ffag061005@gmail.com>
2026-06-15 20:35:41 +0700

    add static error pages

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Server Error</title>
+</head>
+<body>
+    <h1>Server Error</h1>
+    <p>Please check the application logs.</p>
+</body>
+</html>
