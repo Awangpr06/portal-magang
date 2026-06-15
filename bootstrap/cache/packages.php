88ebf072e0367eef92cac1edf311c0a9c863ce97
Awang Pramana <ffag061005@gmail.com>
2026-06-16 00:21:14 +0700

    install collision in production and refresh manifest

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -46,18 +46,18 @@
       0 => 'Carbon\\Laravel\\ServiceProvider',
     ),
   ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
   'nunomaduro/termwind' => 
   array (
     'providers' => 
     array (
       0 => 'Termwind\\Laravel\\TermwindServiceProvider',
-    ),
-  ),
-  'pestphp/pest-plugin-laravel' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Pest\\Laravel\\PestServiceProvider',
     ),
   ),
   'spatie/laravel-ignition' => 
